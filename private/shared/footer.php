596b387b9ec27e3cd6ba1ad2841b3a1ed0b6da67
DeMario Windom <iid3mario@gmail.com>
2018-05-19 14:42:23 -0400

    Footer with social media icons

--- private/shared/footer.php
+++ private/shared/footer.php
@@ -1,8 +1,41 @@
 
 </div><!-- content container -->
-<footer>
+<footer class="footerbackground p-md-4">
+    <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+    <div class="row">
+        <div class="col-md-4 col-sm-6 footer-col">
+            <h5>Timeless Accounting &amp; Tax Services</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
 
-    <p>&copy;<?php echo date('Y'); ?> Timless Accounting &amp; Tax Service Website developed by: <a href="http://www.d3mario.com">DeMario </a></p>
+        </div>
+        <div class="col-md-4 col-sm-6 footer-col">
+            <h5>Contact Us</h5>
+            <ul class="list-unstyled quick-links">
+                <li><a href="javascript:void();"><i class="fa fa-map-pin"></i>28865 Plymouth Rd.. Livonia, MI 48150</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-phone"></i>Toll Free: (877) 70-TAX-TIME</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-phone"></i>Telephone: (734) 744-8456</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-fax"></i>Fax:  (248) 671-0333</a></li>
+            </ul>
+            <ul class="footer-social">
+                <li><i class="fa fa-linkedin social-icon linked-in" aria-hidden="true"></i></li>
+                <li><i class="fa fa-facebook social-icon facebook" aria-hidden="true"></i></li>
+                <li><i class="fa fa-twitter social-icon twitter" aria-hidden="true"></i></li>
+                <li><i class="fa fa-google-plus social-icon google" aria-hidden="true"></i></li>
+            </ul>
+        </div>
+        <div class="col-md-4 col-sm-6 footer-col"><h5>Latest Post</h5>
+            <ul class="list-unstyled quick-links">
+                <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+
+            </ul>
+        </div>
+
+    </div>
+    <p class="copywrite ">&copy;<?php echo date('Y'); ?> Timless Accounting &amp; Tax Service | Website developed by: <a href="http://www.d3mario.com">DeMario </a></p>
 </footer>
 <!--Javascript Declorations: Bootstrap-->
 <script src="../public/js/jquery.slim.min.js"></script>
